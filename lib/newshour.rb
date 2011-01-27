6fd6bb8a00e6e225448378e05eb3ac7d1ea1fd20
Kim Eunsoo <hahah@kaist.ac.kr>
2011-01-27 18:03:28 +0900

    end

--- lib/newshour.rb
+++ lib/newshour.rb
@@ -2,13 +2,20 @@ require 'rubygems'
 require 'mechanize'
 
 class Newshour
-	def get_scripts p
-	end
 	def down
 		m = Mechanize.new
 		p = m.get 'http://www.pbs.org/newshour/newshour_index.html'
-		script = p.search('//b[@class="otn_heading"]/a').map{|x| x[:href]}
+		script = p.search('//b[@class="otn_heading"]/a').map do |x| 
+			m.get(x[:href]).search('//div[@class="copy"]/p').map(&:text).join("\n")
+		end
 		mp3 = p.search('//span[@class="otn_text"]/a').map{|x| x[:href]}
-		script.map!{|s| m.get(s).search('//div[@class="copy"]/p').map(&:text).join("\n")}
+		puts mp3.length
+		0.upto(mp3.length-1) do |i|
+			puts "Download ##{i}"
+			File.open("script##{i}.txt", 'w') {|f| f.write(script[i]) }
+			m.get(mp3[i]).save_as("mp3##{i}.mp3")
+		end
 	end
 end
+
+Newshour.new.down
